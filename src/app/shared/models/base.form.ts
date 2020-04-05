ae53a9db4b0f109c9fe2e2e46084743ae9f990ed
Stefan Ovezea <sovezea@gmail.com>
2020-04-05 11:22:13 +0300

    Added Base Form and modified tslint

---
+++ src/app/shared/models/base.form.ts
@@ -0,0 +1,11 @@
+import { AbstractControl, FormGroup } from '@angular/forms';
+
+export abstract class BaseForm<T> extends FormGroup {
+  protected constructor(form: BaseFormModel<T>) {
+    super(form);
+  }
+}
+
+export type BaseFormModel<T> = {
+  [key in keyof T]?: AbstractControl;
+};
