3674e504e9c6e0a4a831770f217ce30234ddc13d
Adrian Petcu <adipetcu@yahoo.com>
2021-01-23 22:07:04 +0200

    Fixed phone regex

--- src/app/layout/cart/containers/cart-summary/cart-summary-form.ts
+++ src/app/layout/cart/containers/cart-summary/cart-summary-form.ts
@@ -17,7 +17,7 @@ export class CartSummaryForm extends BaseForm<CartSummaryFields> {
   constructor() {
     super({
       email: new FormControl('', [Validators.email, Validators.required]),
-      phone: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required, Validators.pattern('^[0-9.-/]+$')]),
       location: new FormControl('', [Validators.required]),
       shippingAddress: new FormControl('', [Validators.required]),
       remarks: new FormControl(''),
